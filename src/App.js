2ed3fac42e61ced0bba63496bb9406d8e057e561
Charles Takang <charles.takang@gmail.com>
2022-05-02 06:59:05 -0400

    Started on main apps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react'
-
+// import { SeasonDisplay } from './components/SeasonDisplay'
 
 class App extends React.Component{
-    
+
+    state = {
+        lat: null,
+        error: '' }
+
+    componentDidMount(){
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                var crd = position.coords
+                this.setState({lat: crd.latitude})
+            },
+            (err) =>{
+                this.setState({error: err.message})
+            })
+
+    }
+
     render(){
-        return <h1>App</h1>
+        console.log(this.state.lat)
+
+        return (
+            // this.state.lat? this.state.lat : this.state.error
+        
+        <h1>{this.state.lat? this.state.lat : this.state.error}</h1>
+        )
     }
 }
 
